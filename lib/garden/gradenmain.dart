e23a88d5b16d69d1f23c37cd751d221b4f2d809a
Hajin <22100228@handong.ac.kr>
2024-01-29 11:00:52 +0900

    update graden)

--- lib/garden/gradenmain.dart
+++ lib/garden/gradenmain.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:andand/widget/bottomNavi.dart';
-import 'util/color.dart';
+import '../util/color.dart';
 
 class GradenMain extends StatefulWidget {
   const GradenMain({Key? key}) : super(key: key);
@@ -10,20 +10,22 @@ class GradenMain extends StatefulWidget {
 }
 
 class _GradenMainState extends State<GradenMain> {
-  bool _onClickPlus = false;
-  bool _onClickQuestion = false;
 
   void _checkPlus() {
     setState(() {
-      _onClickPlus = true;
       // '+' 버튼 누르면 다른 로 이동 또는 작업 수행
+    });
+  }
+
+  void _checkBook(){
+    setState(() {
+
     });
   }
 
   void _checkQuestion() {
     setState(() {
-      _onClickQuestion = true;
-      // 물음표 버튼 누르면 다른 로 이동 또는 작업 수행
+
     });
   }
 
@@ -43,7 +45,8 @@ class _GradenMainState extends State<GradenMain> {
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(appBarHeight + statusBarHeight),
         child: AppBar(
-          backgroundColor: Colors.white,
+          backgroundColor: Colors.transparent, // 색깔 없음
+          elevation: 0, // 그림자 없음
           title: Padding(
             padding: const EdgeInsets.only(top: 30.0),
             child: Center(
@@ -87,11 +90,17 @@ class _GradenMainState extends State<GradenMain> {
       body: SafeArea(
         child: Stack(
           children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/graden1.jpg'),
-                  fit: BoxFit.cover,
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),  // 왼쪽 상단 모서리 둥글게
+                topRight: Radius.circular(30.0), // 오른쪽 상단 모서리 둥글게
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/graden1.jpg'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
@@ -103,16 +112,32 @@ class _GradenMainState extends State<GradenMain> {
                 onPressed: _checkQuestion,
               ),
             ),
+            Positioned(
+              bottom: 20.0,
+              right: MediaQuery.of(context).size.width * 0.05,
+              child: GestureDetector(
+                onTap: _checkBook,
+                child: Image.asset(
+                  'assets/flowerbook.png',
+                  width: 60.0,
+                  height: 60.0,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 25.0,
+              left: MediaQuery.of(context).size.width * 0.05,
+              child: FloatingActionButton(
+                onPressed: _checkPlus,
+                child: Icon(Icons.add),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                backgroundColor: lightColorScheme.primary,
+              ),
+            ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _checkPlus,
-        child: Icon(Icons.add),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-        backgroundColor: lightColorScheme.primary,
       ),
       bottomNavigationBar: BottomNavi(
         selectedIndex: 0,
