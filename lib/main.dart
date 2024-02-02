5f3cf29b49a43be3b550c5cecdc99fec21afaa16
ParkMinjun0721 <you@example.com>
2024-02-02 13:36:33 +0900

    pmj

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,23 @@ import 'package:andand/login/login_connect_complete.dart';
 import 'package:andand/login/login_main.dart';
 import 'package:andand/login/login_register.dart';
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'util/color.dart';
 import 'home.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:andand/login/kakao_login.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  // KakaoLogin
+  KakaoSdk.init(
+    nativeAppKey: '639d8451a911fbbf1d4282c34b2d9a3f',
+    javaScriptAppKey: '9b3faf53240322a67c32181be7ac8807',
+  );
+
   runApp(const MyApp());
 }
 
@@ -15,7 +28,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ChangeNotifierProvider(
+        create: (_) => UserController(
+          kakaoLoginApi: KakaoLoginApi(),
+        ),
+    child: MaterialApp(
         localizationsDelegates: [
           GlobalCupertinoLocalizations.delegate,
           GlobalMaterialLocalizations.delegate,
@@ -29,7 +46,8 @@ class MyApp extends StatelessWidget {
         primaryColor: lightColorScheme.primary,
         useMaterial3: true,
       ),
-      home: const LoginCodeConnect(),
+      home: const App(),
+      ),
     );
   }
 }
