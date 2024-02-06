767f87a1f470f55dfe1e8fe64305cfaa997cc45c
Hajin <22100228@handong.ac.kr>
2024-02-06 11:23:05 +0900

    update khj

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'firebase_options.dart';
 import 'splash_page.dart';
 import 'util/color.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:andand/login/kakao_login.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  // KakaoLogin
-  KakaoSdk.init(
-    nativeAppKey: '639d8451a911fbbf1d4282c34b2d9a3f',
-    javaScriptAppKey: '9b3faf53240322a67c32181be7ac8807',
-  );
+    // KakaoLogin
+    KakaoSdk.init(
+      nativeAppKey: '639d8451a911fbbf1d4282c34b2d9a3f',
+      javaScriptAppKey: '9b3faf53240322a67c32181be7ac8807',
+    );
+    runApp(const MyApp());
+  } catch (e) {
+    print("Error during Firebase initialization: $e");
+  }
 
   runApp(const MyApp());
 }
