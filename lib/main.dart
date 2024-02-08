061fdae8296d20d0c3b840530ec3c237265bb9f6
ParkMinjun0721 <you@example.com>
2024-02-08 11:05:00 +0900

    pmj

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:andand/home.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
@@ -53,7 +52,7 @@ class MyApp extends StatelessWidget {
         primaryColor: lightColorScheme.primary,
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const SplashScreen(),
     ),
     );
   }
